524c258e8e92800ca2967d294b78611b51323a97
Anton Kirschhock <anton.kirschhock@gmail.com>
2016-11-28 21:38:50 +0100

    still buggy (timing issues?)

--- TestDriver/StartupTask.cs
+++ TestDriver/StartupTask.cs
@@ -23,31 +23,50 @@ namespace TestDriver
                 var factory = DeviceFactory.Build;
                 using (var gopigo = factory.BuildGoPiGo())
                 {
-                    var ultra = factory.BuildUltraSonicSensor(Pin.Analog1);
-                    var led = factory.BuildLed(Pin.LedLeft);
-                    gopigo.MotorController().MoveForward();
-                    await Task.Delay(1000);
-                    led.ChangeState(SensorStatus.On);
-                    gopigo.MotorController().RotateLeft();
-                    await Task.Delay(1000);
-                    led.ChangeState(SensorStatus.Off);
-                    gopigo.MotorController().RotateRight();
-                    await Task.Delay(1000);
-                    led.ChangeState(SensorStatus.On);
-                    gopigo.MotorController().MoveBackward();
-                    await Task.Delay(1000);
-                    led.ChangeState(SensorStatus.Off);
-                    gopigo.MotorController().Stop();
+                    var led1 = factory.BuildLed(Pin.LedLeft);
+                    var led2 = factory.BuildLed(Pin.LedRight);
+                    led1.ChangeState(GoPiGo.Sensors.SensorStatus.On);
+                    await Task.Delay(20);
+                    led2.ChangeState(GoPiGo.Sensors.SensorStatus.On);
+                    await Task.Delay(1500);
+                    led1.ChangeState(GoPiGo.Sensors.SensorStatus.Off);
+                    await Task.Delay(20); 
+                    led2.ChangeState(GoPiGo.Sensors.SensorStatus.Off);
+
+                    DateTime dtStart = DateTime.Now;
+                    var ultrasonic = factory.BuildUltraSonicSensor(Pin.Analog1);
                     gopigo.MotorController().EnableServo();
-                    for (int i = 0; i < 180; i+=10)
-                    {
-                        gopigo.MotorController().RotateServo(i);
-                        await Task.Delay(1000);
-                    }
                     gopigo.MotorController().RotateServo(90);
                     gopigo.MotorController().DisableServo();
-                    System.Diagnostics.Debug.WriteLine($"Distance: {ultra.MeasureInCentimeters()}cm");
-                    await Task.Delay(1000);
+                    while ((DateTime.Now - dtStart).TotalMinutes < 5)
+                    {
+                        System.Diagnostics.Debug.WriteLine("battery: " + gopigo.BatteryVoltage());
+                        gopigo.MotorController().Stop();
+                        gopigo.MotorController().MoveForward();
+                        await Task.Delay(50);
+                        var d = ultrasonic.MeasureInCentimeters();
+                        while (d > 30)
+                        {
+                            await Task.Delay(500);
+                            d = ultrasonic.MeasureInCentimeters();
+                            System.Diagnostics.Debug.WriteLine($"Driving forward with {d} cm of space");
+
+
+                        }
+                        gopigo.MotorController().Stop();
+
+                        while (d < 30)
+                        {
+                            led1.ChangeState(GoPiGo.Sensors.SensorStatus.On);
+                            gopigo.MotorController().RotateLeft();
+                            await Task.Delay(250);
+                            gopigo.MotorController().Stop();
+                            led1.ChangeState(GoPiGo.Sensors.SensorStatus.Off);
+                            await Task.Delay(500);
+                            d = ultrasonic.MeasureInCentimeters();
+                            await Task.Delay(10);
+                        }
+                    }
                 }
             }).Wait();
         }
