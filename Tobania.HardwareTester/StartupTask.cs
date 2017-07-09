570e91707279010a525a55e41b37bec4f732a73c
Harold Pulcher <pulcher@pulcher.biz>
2017-07-09 10:26:54 -0500

    commented out the server stuff that I am not using at the moment

--- Tobania.HardwareTester/StartupTask.cs
+++ Tobania.HardwareTester/StartupTask.cs
@@ -1,15 +1,8 @@
 ﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net.Http;
-using System.Threading;
-using Windows.ApplicationModel.Background;
-using Windows.System.Threading;
 using GoPiGo;
-using GoPiGo.Sensors;
+using System;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -39,21 +32,24 @@ namespace Tobania.HardwareTester
                     System.Diagnostics.Debug.WriteLine("Done!\n");
 
 
-                    System.Diagnostics.Debug.WriteLine("Testing servo");
-                    gpg.MotorController().EnableServo();
-                    await Task.Delay(100);
-                    gpg.MotorController().RotateServo(0);
-                    await Task.Delay(1500);
-                    gpg.MotorController().RotateServo(180);
-                    await Task.Delay(1500);
-                    gpg.MotorController().DisableServo();
-                    System.Diagnostics.Debug.WriteLine("Done!\n");
-                    await Task.Delay(100);
-                    System.Diagnostics.Debug.WriteLine("Creating ultrasonic sensor...");
-                    var ultrasonic = factory.BuildUltraSonicSensor(Pin.Analog1);
-                    await Task.Delay(100);
-                    System.Diagnostics.Debug.WriteLine($"\tDistance measured: {ultrasonic.MeasureInCentimeters()} cm");
-                    System.Diagnostics.Debug.WriteLine("Done!\n");
+                    //System.Diagnostics.Debug.WriteLine("Testing servo");
+                    //gpg.MotorController().EnableServo();
+                    //await Task.Delay(100);
+                    //gpg.MotorController().RotateServo(0);
+                    //await Task.Delay(1500);
+                    //gpg.MotorController().RotateServo(180);
+                    //await Task.Delay(1500);
+                    //gpg.MotorController().DisableServo();
+                    //System.Diagnostics.Debug.WriteLine("Done!\n");
+                    //await Task.Delay(100);
+                    //System.Diagnostics.Debug.WriteLine("Creating ultrasonic sensor...");
+                    //var ultrasonic = factory.BuildUltraSonicSensor(Pin.Analog1);
+                    //await Task.Delay(100);
+                    //System.Diagnostics.Debug.WriteLine($"\tDistance measured: {ultrasonic.MeasureInCentimeters()} cm");
+                    //System.Diagnostics.Debug.WriteLine("Done!\n");
+
+                    //// wait for the thing to rest
+                    //await Task.Delay(100);
 
                     System.Diagnostics.Debug.WriteLine("Testing motor controls...");
 
@@ -62,7 +58,7 @@ namespace Tobania.HardwareTester
                     await Task.Delay(5000);
 
                     System.Diagnostics.Debug.WriteLine("\tBackwards for 5seconds");
-                    gpg.MotorController().MoveForward();
+                    gpg.MotorController().MoveBackward();
                     await Task.Delay(5000);
 
                     System.Diagnostics.Debug.WriteLine("\tMoving left for 5seconds");
